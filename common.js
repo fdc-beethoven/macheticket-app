93206caf2390aa11ca6178d5724d8cf211396600
fdc-beethoven <143068925+fdc-beethoven@users.noreply.github.com>
2024-08-29 09:29:28 +0800

    Added files for directory structure change

---
+++ common.js
@@ -0,0 +1,24 @@
+const jiraUser = process.env.JIRA_USERNAME;
+const jiraToken = process.env.JIRA_TOKEN;
+const jiraHeaders = {
+  Accept: "application/json",
+  Authorization: `Basic ${Buffer.from(`${jiraUser}:${jiraToken}`).toString("base64")}`,
+};
+
+const slackIdRegex = /<@(.+?)>/;
+
+const leadsUserId = {
+  ios_lead: process.env.IOS_LEAD_ID,
+  android_lead: process.env.ANDROID_LEAD_ID,
+  talk_web_lead: process.env.TALK_WEB_LEAD_ID,
+  qa_lead: process.env.QA_LEAD_ID,
+  lulu_web_lead: process.env.LULU_WEB_LEAD_ID,
+};
+
+const pmUserId = process.env.PM_USER_ID;
+
+module.exports = {
+  jiraHeaders,
+  slackIdRegex,
+  leadsUserId
+};
---
+++ utils/services.js
@@ -0,0 +1,112 @@
+const axios = require("axios");
+const common = require("common");
+
+module.exports = {
+  updateSlackThreadUrl: async function (issueKey, slackUrl) {
+    let payload = {
+      fields: {
+        customfield_10257: slackUrl,
+      },
+    };
+    return axios.put(
+      `https://native-camp.atlassian.net/rest/api/2/issue/${issueKey}`,
+      payload,
+      { headers: common.jiraHeaders }
+    );
+  },
+  fetchJiraIssue: async function (issueKey) {
+    const response = await axios.get(
+      `https://native-camp.atlassian.net/rest/api/2/issue/${issueKey}`,
+      { headers: common.jiraHeaders }
+    );
+    const jiraData = response.data;
+    let displayNameBE = jiraData.fields.customfield_10033
+      ? jiraData.fields.customfield_10033.displayName
+      : null;
+
+    function getSlackIdBE(displayNameBE) {
+      switch (displayNameBE) {
+        case "FDC.Beethoven-MT-BE":
+          return "U05MHT6UWS1";
+        case "FDC.Julie-MT-BE":
+          return "UBJUTB5AQ";
+        case "FDC.Sakai-MT-Web/BE":
+          return "U06A5K08K2L";
+        case "Ko Inoue":
+          return "U05ST3F2FGU";
+        default:
+          return "None";
+      }
+    }
+
+    return {
+      issueKey: jiraData.key,
+      summary: jiraData.fields.summary,
+      assignedBE: getSlackIdBE(displayNameBE),
+      priority: jiraData.fields.priority.name,
+      slackUrl: jiraData.fields.customfield_10257,
+    };
+  },
+  getJiraComponents: async function () {
+    let url =
+      "https://native-camp.atlassian.net/rest/api/2/project/MCT/components";
+
+    const response = await axios.get(url, {
+      headers: common.jiraHeaders,
+    });
+
+    return response.data.map(({ name }) => name);
+  },
+  updateJiraSubtasks: async function (issueKey, action_item, platform_array, deadline) {
+    // Step 1: Construct JQL query
+    const baseJQL = `project = MCT AND type = Sub-task AND parent = ${issueKey} AND summary ~ "${action_item}"`;
+
+    const platformMapping = {
+      Android: 'summary ~ "android"',
+      iOS: 'summary ~ "ios"',
+      Web: '(summary ~ "admin" OR summary ~ "api" OR summary ~ "db" OR summary ~ "cron" OR summary ~ "front" OR summary ~ "pc" OR summary ~ "SP")',
+    };
+
+    const platformConditions = platform_array.map(
+      (platform) => platformMapping[platform]
+    );
+    const platformJQL = platformConditions.join(" OR ");
+    const fullJQL = `${baseJQL} AND (${platformJQL}) ORDER BY created DESC`;
+    const encodedJQLquery = encodeURIComponent(fullJQL);
+    console.log(encodedJQLquery);
+
+    // Step 2: Get Jira subtasks
+    const jiraUrl = `https://native-camp.atlassian.net/rest/api/3/search?jql=${encodedJQLquery}`;
+
+    const jiraSubtasksResponse = await axios.get(jiraUrl, {
+      headers: jiraHeaders,
+    });
+    console.log(jiraSubtasksResponse);
+    const jiraSubtasks = jiraSubtasksResponse.data;
+    const subtaskIssueKeys = jiraSubtasks.issues.map((subtask) => subtask.key);
+    console.log(subtaskIssueKeys);
+
+    // Step 3: Update Jira subtasks
+    const payload = {
+      editedFieldsInput: {
+        datePickerFields: [
+          {
+            date: {
+              formattedDate: `${deadline}`,
+            },
+            fieldId: "customfield_10023",
+          },
+        ],
+      },
+      selectedActions: ["customfield_10023"],
+      selectedIssueIdsOrKeys: subtaskIssueKeys,
+    };
+
+    const updateResponse = await axios.post(
+      "https://native-camp.atlassian.net/rest/api/3/bulk/issues/fields",
+      payload,
+      { headers: jiraHeaders }
+    );
+    return updateResponse;
+  },
+};
