124f3ffb54c65e72d168d6813044a5e46096a936
fdc-beethoven <143068925+fdc-beethoven@users.noreply.github.com>
2024-09-12 17:11:31 +0800

    Separate handler for post ticket

--- controllers/command.js
+++ controllers/command.js
@@ -2,6 +2,65 @@ const HELPER = require("../utils/helpers");
 const COMMON = require("../utils/common");
 const SERVICE = require("../utils/services");
 
+async function handlePostTicket({ command, ack, respond, client }) {
+    await ack();
+    const jiraBaseUrl = "https://native-camp.atlassian.net/browse/";
+    let requestKey = command.text.trim();
+    let channelId = command.channel_id;
+    try {
+      HELPER.validateKey(requestKey);
+      let jiraDetails = await SERVICE.fetchJiraIssue(requestKey);
+      let jiraIssueLink = jiraBaseUrl + requestKey;
+      let payloadText = `<!here> *${jiraDetails.issueKey} ${jiraDetails.summary}* \`\`\`JIRA: <${jiraIssueLink}|${jiraIssueLink}>\`\`\``;
+      let postResponse = await client.chat.postMessage({
+        channel: channelId,
+        text: payloadText,
+        attachments: [
+          {
+            id: 1,
+            color: "3AA3E3",
+            fallback: "Estimation unsuccessful.",
+            callback_id: "estimation",
+            fields: [
+              {
+                value: `<@${jiraDetails.assignedBE}>`,
+                title: "AssignedBE:",
+                short: true,
+              },
+              {
+                value: `:${jiraDetails.priority}:  ${jiraDetails.priority}`,
+                title: "Priority:",
+                short: true,
+              },
+            ],
+            actions: [
+              {
+                id: "click_estimate",
+                name: "estimate",
+                text: "Estimate",
+                type: "button",
+                value: "estimate_clicked",
+                style: "",
+              },
+            ],
+          },
+        ],
+      });
+      if (postResponse.ok) {
+        let updateResponse = await SERVICE.updateSlackThreadUrl(jiraDetails.issueKey,`https://fdcinc.slack.com/archives/${postResponse.channel}/p${postResponse.ts.replace(".", "")}`);
+        respond({
+          response_type: "ephemeral",
+          text: "Ticket has been posted~",
+        });
+      }
+    } catch (error) {
+      await respond({
+        response_type: "ephemeral",
+        text: error.message,
+      });
+    }
+  }
+
 async function handleEstimate({ command, ack, respond, client }) {
   await ack();
   let requestKey = command.text.trim();
@@ -31,4 +90,4 @@ async function handleEstimate({ command, ack, respond, client }) {
   }
 }
 
-module.exports = { handleEstimate }
+module.exports = { handlePostTicket, handleEstimate }
--- server.js
+++ server.js
@@ -18,64 +18,7 @@ const app = new App({
   receiver: expressReceiver
 });
 // ========== ENTER BOLT CODE AFTER THIS LINE  ==========
-app.command("/post-ticket", async ({ command, ack, respond, client }) => {
-  await ack();
-  const jiraBaseUrl = "https://native-camp.atlassian.net/browse/";
-  let requestKey = command.text.trim();
-  let channelId = command.channel_id;
-  try {
-    HELPER.validateKey(requestKey);
-    let jiraDetails = await SERVICE.fetchJiraIssue(requestKey);
-    let jiraIssueLink = jiraBaseUrl + requestKey;
-    let payloadText = `<!here> *${jiraDetails.issueKey} ${jiraDetails.summary}* \`\`\`JIRA: <${jiraIssueLink}|${jiraIssueLink}>\`\`\``;
-    let postResponse = await client.chat.postMessage({
-      channel: channelId,
-      text: payloadText,
-      attachments: [
-        {
-          id: 1,
-          color: "3AA3E3",
-          fallback: "Estimation unsuccessful.",
-          callback_id: "estimation",
-          fields: [
-            {
-              value: `<@${jiraDetails.assignedBE}>`,
-              title: "AssignedBE:",
-              short: true,
-            },
-            {
-              value: `:${jiraDetails.priority}:  ${jiraDetails.priority}`,
-              title: "Priority:",
-              short: true,
-            },
-          ],
-          actions: [
-            {
-              id: "click_estimate",
-              name: "estimate",
-              text: "Estimate",
-              type: "button",
-              value: "estimate_clicked",
-              style: "",
-            },
-          ],
-        },
-      ],
-    });
-    if (postResponse.ok) {
-      let updateResponse = await SERVICE.updateSlackThreadUrl(jiraDetails.issueKey,`https://fdcinc.slack.com/archives/${postResponse.channel}/p${postResponse.ts.replace(".", "")}`);
-      respond({
-        response_type: "ephemeral",
-        text: "Ticket has been posted~",
-      });
-    }
-  } catch (error) {
-    await respond({
-      response_type: "ephemeral",
-      text: error.message,
-    });
-  }
-});
+app.command("/post-ticket", COMMAND.handlePostTicket);
 
 app.command("/estimate", COMMAND.handleEstimate);
 
